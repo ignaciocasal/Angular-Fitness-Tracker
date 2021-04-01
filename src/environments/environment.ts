ce463d44ab2dca0b217f027741d887feaca325a9
Ignacio Casal <ignaciocasaldev@gmail.com>
2021-04-01 15:04:17 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBS8je36EpPd0K6haAkdoS8T3Y9az4WzSY",
-    authDomain: "angularfitnesstracker-60f50.firebaseapp.com",
-    databaseURL: "https://angularfitnesstracker-60f50.firebaseio.com",
-    projectId: "angularfitnesstracker-60f50",
-    storageBucket: "angularfitnesstracker-60f50.appspot.com",
-    messagingSenderId: "174869053101",
-    appId: "1:174869053101:web:7850e314e0140076281024"
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    appId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
   }
 };
 
